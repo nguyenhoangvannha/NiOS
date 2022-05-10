1b463899987fc8ac2965f7bf9cd72a4995587647
nguyenhoangvannha <nguyenhoangvannha@gmail.com>
2022-05-10 22:03:11 +0700

    Remove withCheckedThrowingContinuation

--- NhaDoiOS/NhaDoiOS/Data/DataSource/TodoStore.swift
+++ NhaDoiOS/NhaDoiOS/Data/DataSource/TodoStore.swift
@@ -17,31 +17,18 @@ class TodoStore {
     }
     
     func load() async throws -> [Todo] {
-        return try await withCheckedThrowingContinuation { continuation in
-            do {
-                let fileURL1 = try self.fileURL()
-                guard let file = try? FileHandle(forReadingFrom: fileURL1) else {
-                    continuation.resume(with: .success([]))
-                    return
-                }
-                let todos = try JSONDecoder().decode([Todo].self, from: file.availableData)
-                continuation.resume(with: .success(todos))
-            } catch {
-                continuation.resume(with: .failure(error))
-            }
+        let fileURL1 = try self.fileURL()
+        guard let file = try? FileHandle(forReadingFrom: fileURL1) else {
+            return []
         }
+        let todos = try JSONDecoder().decode([Todo].self, from: file.availableData)
+        return todos
     }
     
     func save(todos: [Todo]) async throws -> Int {
-        return try await withCheckedThrowingContinuation { continuation in
-            do {
-                let data = try JSONEncoder().encode(todos)
-                let outfile = try self.fileURL()
-                try data.write(to: outfile)
-                continuation.resume(with: .success(todos.count))
-            } catch {
-                continuation.resume(with: .failure(error))
-            }
-        }
+        let data = try JSONEncoder().encode(todos)
+        let outfile = try self.fileURL()
+        try data.write(to: outfile)
+        return todos.count
     }
 }
