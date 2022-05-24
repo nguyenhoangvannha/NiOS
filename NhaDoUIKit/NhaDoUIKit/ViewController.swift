538e668683244843b5dd0dc495dc9566ba641ec6
nguyenhoangvannha <nguyenhoangvannha@gmail.com>
2022-05-24 21:33:21 +0700

    Init UIKIT project

---
+++ NhaDoUIKit/NhaDoUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NhaDoUIKit
+//
+//  Created by Nguyễn Hoàng Văn Nhã on 5/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
