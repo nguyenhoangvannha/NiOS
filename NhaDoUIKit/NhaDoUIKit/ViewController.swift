82ad6c5f2b115ef4c74e4da787323198d55e81e8
nguyenhoangvannha <nguyenhoangvannha@gmail.com>
2022-06-07 21:30:52 +0700

    UIKit storyboard view creation

--- NhaDoUIKit/NhaDoUIKit/ViewController.swift
+++ NhaDoUIKit/NhaDoUIKit/ViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var scoreLabel: UILabel!
-    
-    override func loadView() {
-        view = UIView()
-           view.backgroundColor = .white
-        scoreLabel = UILabel()
-        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
-        scoreLabel.textAlignment = .right
-        scoreLabel.text = "Daily Todos"
-        view.addSubview(scoreLabel)
-        
-        NSLayoutConstraint.activate([
-                scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
-                scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 0),
-            ])
-    }
+//    var scoreLabel: UILabel!
+//
+//    override func loadView() {
+//        view = UIView()
+//           view.backgroundColor = .white
+//        scoreLabel = UILabel()
+//        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+//        scoreLabel.textAlignment = .right
+//        scoreLabel.text = "Daily Todos"
+//        view.addSubview(scoreLabel)
+//
+//        NSLayoutConstraint.activate([
+//                scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+//                scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 0),
+//            ])
+////
+////        let controller:TodoCardViewController = todoCardStoryBoard!.instantiateViewController(withIdentifier: "TodoCard") as! TodoCardViewController
+////
+////        controller.view.frame = self.view.bounds
+////        self.view.addSubview(controller.view)
+////        self.addChild(controller)
+////        controller.didMove(toParent: self)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
