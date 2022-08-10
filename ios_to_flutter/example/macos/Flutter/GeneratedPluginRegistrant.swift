bcdaeba8e735f576335b9321563ac883034e5393
nguyenhoangvannha <nguyenhoangvannha@gmail.com>
2022-08-10 13:35:27 +0700

    ios_to_flutter init example

---
+++ ios_to_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import ios_to_flutter
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  IosToFlutterPlugin.register(with: registry.registrar(forPlugin: "IosToFlutterPlugin"))
+}
